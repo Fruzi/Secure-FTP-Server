baa451512df66c3666c49231c7f48cf75ff91065
Uzi <uzifr@post.bgu.ac.il>
2018-05-29 20:44:18 +0300

    Implemented a smarter version of the authorizer, needs further testing of all features except register, login, mkd. Clean up code to not go crazy over existing folders, users, etc.

--- server/db.py
+++ server/db.py
@@ -1,17 +1,21 @@
 import sqlite3
 import os
+import json
 
 
 def main():
-    # create_user_file()
+    # create_user_metadata()
     # create_tag_file()
     # add_user("Rawn", "a3nme", "123456")
     # add_user("Uzi", "1245", "anjkf")
     # print(fetch_tag("uzi's file"))
     # print(fetch_tag("few"))
-    print(fetch_user("banana"))
-    print(has_user("Uzi"))
-    print(has_user("banana"))
+    # print(fetch_user("banana"))
+    # print(has_user("Uzi"))
+    # print(has_user("banana"))
+    # add_user_metadata("v", '', '', {'p': '5'}, '', '')
+    print(fetch_user_metadata("VVVV")[1])
+    # print(has_user("Amit"))
 
 
 # Check if userfile exists, if not then create it.
@@ -23,7 +27,7 @@ def create_user_file():
             cursor.execute("""CREATE TABLE Users (
                             username TEXT PRIMARY KEY NOT NULL,
                             salt TEXT NOT NULL,
-                            hashed_pass TEXT NOT NULL)""")
+                            hashed_pass BLOB NOT NULL)""")
 
 
 # check if tagfile exits, if not then create it.
@@ -37,12 +41,61 @@ def create_tag_file():
                             tag TEXT NOT NULL)""")
 
 
+def create_user_metadata():
+    metadata_existed = os.path.isfile('user_metadata.db')
+    with sqlite3.connect('user_metadata.db') as dbcon:
+        cursor = dbcon.cursor()
+        if not metadata_existed:
+            cursor.execute("""CREATE TABLE Metadata (
+                            username TEXT PRIMARY KEY NOT NULL,
+                            homedir TEXT NOT NULL,
+                            perm TEXT NOT NULL,
+                            operms NOT NULL,
+                            msg_login TEXT NOT NULL,
+                            msg_quit TEXT NOT NULL)""")
+
+
+def add_user_metadata(username, homedir, perm, operms, msg_login, msg_quit):
+    with sqlite3.connect('user_metadata.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""INSERT INTO Metadata VALUES (?, ?, ?, ?, ?, ?)""",
+                       (username, homedir, perm, json.dumps(operms), msg_login, msg_quit))
+        return cursor.lastrowid
+
+
+def remove_user_metadata(username):
+    with sqlite3.connect('user_metadata.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""DELETE FROM Metadata WHERE username = (?)""", (username,))
+        return cursor.lastrowid
+
+
+def fetch_user_metadata(username):
+    with sqlite3.connect('user_metadata.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""SELECT homedir, perm, msg_login, msg_quit FROM Metadata WHERE username = (?)""", (username,))
+        return cursor.fetchone()
+
+
+def fetch_operms(username):
+    with sqlite3.connect('user_metadata.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""SELECT operms FROM Metadata WHERE username = (?)""", (username,))
+        return json.loads(cursor.fetchone()[0])
+
+
 # Adds a user to the userfile. Expects to receive a salted password post hashing.
 def add_user(_name, _salt, _pass):
     with sqlite3.connect('users.db') as dbcon:
         cursor = dbcon.cursor()
         cursor.execute("""INSERT INTO Users VALUES (?,?,?)""", (_name, _salt, _pass))
         return cursor.lastrowid
+
+
+def remove_user(_name):
+    with sqlite3.connect('users.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""DELETE FROM Users WHERE username = (?)""", (_name,))
 
 
 # Returns a tuple contains the salt and hashed password of a given username
@@ -65,6 +118,12 @@ def add_tag(_filename, _tag):
         cursor = dbcon.cursor()
         cursor.execute("""INSERT INTO Tags VALUES (?,?)""", (_filename, _tag))
         return cursor.lastrowid
+
+
+def remove_tag(_filename):
+    with sqlite3.connect('tags.db') as dbcon:
+        cursor = dbcon.cursor()
+        cursor.execute("""DELETE FROM Tags Where filename = (?)""", (_filename,))
 
 
 # Returns a tuple with the tag for the given (encrypted) filename.
--- server/server.py
+++ server/server.py
@@ -1,8 +1,111 @@
 import os
 import logging
+import db
+import pyftpdlib.filesystems
 from pyftpdlib.authorizers import DummyAuthorizer
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
 from pyftpdlib.handlers import FTPHandler, proto_cmds
 from pyftpdlib.servers import FTPServer
+
+
+class MySmartyAuthorizer(DummyAuthorizer):
+
+    def __init__(self):
+        db.create_user_file()
+        db.create_tag_file()
+        db.create_user_metadata()
+
+    def add_user(self, username, password, homedir, perm='elr',
+                 msg_login="Login successful.", msg_quit="Goodbye."):
+        if self.has_user(username):
+            raise ValueError('user %r already exists' % username)
+        if not isinstance(homedir, pyftpdlib.filesystems.unicode):
+            homedir = homedir.decode('utf8')
+        if not os.path.isdir(homedir):
+            raise ValueError('no such directory: %r' % homedir)
+        homedir = os.path.realpath(homedir)
+        self._check_permissions(username, perm)
+        salt = os.urandom(16)
+        kdf = Scrypt(
+            salt=salt,
+            length=32,
+            n=2**14,
+            r=8,
+            p=1,
+            backend=default_backend()
+        )
+        key = kdf.derive(bytes.fromhex(password))
+        db.add_user(username, salt, key)
+        db.add_user_metadata(username, homedir, perm, '', msg_login, msg_quit)
+
+    def remove_user(self, username):
+        super.remove_user(username)
+        db.remove_user(username)
+        db.remove_user_metadata(username)
+
+    def validate_authentication(self, username, password, handler):
+        msg = "Authentication failed."
+        if not self.has_user(username):
+            raise Exception(msg)
+        udata = db.fetch_user(username)
+        kdf = Scrypt(
+            salt=udata[0],
+            length=32,
+            n=2**14,
+            r=8,
+            p=1,
+            backend=default_backend()
+        )
+        kdf.verify(bytes.fromhex(password), udata[1])
+
+    def get_home_dir(self, username):
+        return db.fetch_user_metadata(username)[0]
+
+    def has_user(self, username):
+        return db.has_user(username)
+
+    def has_perm(self, username, perm, path=None):
+        """Whether the user has permission over path (an absolute
+        pathname of a file or a directory).
+
+        Expected perm argument is one of the following letters:
+        "elradfmwMT".
+        """
+        if path is None:
+            return perm in self.get_perms(username)
+
+        path = os.path.normcase(path)
+        operms = self.get_operms(username)
+        if not operms:
+            return perm in self.get_perms(username)
+        for dir in operms.keys():
+            operm, recursive = operms[dir]
+            if self._issubpath(path, dir):
+                if recursive:
+                    return perm in operm
+                if (path == dir or os.path.dirname(path) == dir and not
+                        os.path.isdir(path)):
+                    return perm in operm
+        return perm in self.get_perms(username)
+
+    def get_perms(self, username):
+        """Return current user permissions."""
+        return db.fetch_user_metadata(username)[1]
+
+    def get_operms(self, username):
+        return db.fetch_operms(username)
+
+    def get_msg_login(self, username):
+        return db.fetch_user_metadata(username)[2]
+
+    def get_msg_quit(self, username):
+        try:
+            return db.fetch_user_metadata(username)[3]
+        except KeyError:
+            return "Goodbye."
+
+
 
 
 class MyFTPHandler(FTPHandler):
@@ -16,10 +119,7 @@ class MyFTPHandler(FTPHandler):
         super().__init__(conn, server, ioloop)
 
     def ftp_RGTR(self, line):
-        """Register a new user. The logged in user must be the anonymous user."""
-        if self.username != 'anonymous':
-            self.respond("503 Can't register while logged in.")
-            return
+        """Register a new user."""
         if self.authorizer.has_user(line):
             self.respond("503 Username already exists. Choose a different name.")
             return
@@ -37,17 +137,17 @@ class MyFTPHandler(FTPHandler):
         self.flush_account()
         self.username = username
 
+        self.registering = False
+        self.handle_auth_success(username, line, "New USER '%s' registered." % self.username)
         self.fs.mkdir(username)
         self.authorizer.add_user(username, line, username, perm='elradfmwMT')
-        self.log("New USER '%s' registered." % self.username)
-        self.registering = False
-        super().ftp_PASS(line)
+        print("!!!!!!!")
 
 
 def main():
     # TODO: Make secure authorizer
-    authorizer = DummyAuthorizer()
-    authorizer.add_anonymous(os.getcwd(), perm='')
+    authorizer = MySmartyAuthorizer()
+    # authorizer.add_anonymous(os.getcwd(), perm='')
 
     handler = MyFTPHandler
     handler.authorizer = authorizer
